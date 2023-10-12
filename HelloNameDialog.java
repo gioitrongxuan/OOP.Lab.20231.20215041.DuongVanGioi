e77d16e698cb68701e83fbcd6f8d1cdf8882a895
DGioi <gioiduovan@gmail.com>
2023-10-12 17:18:07 +0700

    add answer.txt

--- HelloNameDialog.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.JOptionPane;
-public class HelloNameDialog {
-    public static void main(String[] args) {
-        String result;
-        result=javax.swing.JOptionPane.showInputDialog("Please enter your name:");
-        javax.swing.JOptionPane.showMessageDialog(null,"Hi "+result+"!");
-        System.exit(0);
-    }
-}
--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Xin chao \n cac ban!");
-        System.out.println("Hello World !");
-    }
-}
