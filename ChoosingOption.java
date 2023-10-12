fe87b59badb00dfd0a2a73365ca6d79c99218214
DGioi <gioiduovan@gmail.com>
2023-10-12 21:19:54 +0700

    Complete answer and ChoosingOption program

---
+++ ChoosingOption.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+public class ChoosingOption {
+   
+   public static void main(String[] args) {
+      
+    int option = JOptionPane.showConfirmDialog(null, "Do you wan to change to the first class ticket?",null,JOptionPane.YES_NO_OPTION);
+    JOptionPane.showMessageDialog(null, "You've chosen: "+(option==JOptionPane.YES_OPTION?"Yes":"No"));
+    System.exit(0);
+   } 
+}
+
+
