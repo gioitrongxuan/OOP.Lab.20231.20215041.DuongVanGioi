8d8cd8a85759e2a9aa2cd44621cda55a747d5f8e
DGioi <gioiduovan@gmail.com>
2023-10-12 09:51:43 +0700

    Complete assignment 2.2.2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,4 @@
-public class HelloWorld
-{
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Xin chao \n cac ban!");
         System.out.println("Hello World !");
